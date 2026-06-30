6b385528ac61102730e67157885c9e880c466db2
wildchance_conglomerate <misangoedgaruche@gmail.com>
2026-06-30 08:05:18 +0300

    Delete services/mt5_service.py

--- services/mt5_service.py
+++
@@ -1,3 +0,0 @@
-async def place_order(pair: str, action: str, lot_size: float, price: float):
-    print(f"MOCK TRADE => {pair} | {action} | Lot: {lot_size} | Price: {price}")
-    return {"status": "success", "pair": pair, "action": action}
