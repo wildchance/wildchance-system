9aa37688122b3d5ca3aaecbb1b70e68fc0172367
wildchance_conglomerate <misangoedgaruche@gmail.com>
2026-06-30 08:03:38 +0300

    Delete routes/webhook.py

--- routes/webhook.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, Request
-
-router = APIRouter()
-
-@router.post("/")
-async def webhook_receiver(request: Request):
-    data = await request.json()
-    return {"received": data}
