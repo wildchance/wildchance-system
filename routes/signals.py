2162de69b5ec65c2841d88ddcee2a8ee2567ed3f
wildchance_conglomerate <misangoedgaruche@gmail.com>
2026-06-30 08:03:07 +0300

    Delete routes/signals.py

--- routes/signals.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from models.signal import Signal
-
-router = APIRouter()
-
-@router.post("/")
-def receive_signal(signal: Signal):
-    return {"status": "Signal received", "data": signal}
