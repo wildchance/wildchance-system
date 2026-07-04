60d66a0e27c0b194d233930cf63fde8315e2a306
wildchance_conglomerate <misangoedgaruche@gmail.com>
2026-07-04 14:01:30 +0300

    Add gold router to main application

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ from routes.mmm import router as mmm_router
 from routes.calendar import router as calendar_router
 from routes.edgefinder import router as edgefinder_router
 from routes.backtest import router as backtest_router
+from routes.gold import router as gold_router
 
 # Real-time streaming
 from services.polygon_stream import polygon_stream
@@ -133,6 +134,7 @@ app.include_router(mmm_router)
 app.include_router(calendar_router)
 app.include_router(edgefinder_router)
 app.include_router(backtest_router)
+app.include_router(gold_router)
 app.include_router(correlation_router)
 
 # Serve the static dashboards (EdgeFinder board at /static/dashboard/edgefinder.html).
